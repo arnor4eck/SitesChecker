431beeb8a9cb829d2f741fdc494b189ccd0cfcad
Nikita Kivalin <kivalinnik@mail.ru>
2026-02-10 13:22:31 +0300

    added main application class

--- src/main/java/com/arnor4eck/Main.java
+++ src/main/java/com/arnor4eck/Main.java
@@ -2,6 +2,8 @@ package com.arnor4eck;
 
 public class Main {
     public static void main(String[] args) {
+        SiteCheckerApplication app = new SiteCheckerApplication();
 
+        app.start();
     }
 }
---
+++ src/main/java/com/arnor4eck/SiteCheckerApplication.java
@@ -0,0 +1,9 @@
+package com.arnor4eck;
+
+/** Основной класс приложения
+ * */
+public class SiteCheckerApplication {
+    public void start() {
+
+    }
+}
