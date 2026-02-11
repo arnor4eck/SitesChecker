88be8cb66c237a6d2dea6e1ceaac0d32eef25f9a
Nikita Kivalin <kivalinnik@mail.ru>
2026-02-11 13:27:04 +0300

    created test for InMemorySiteStatisticsStorage

---
+++ src/test/java/MonitoringTaskStorageTest.java
@@ -0,0 +1,73 @@
+import com.arnor4eck.entity.MonitoringTask;
+import com.arnor4eck.storage.MonitoringTaskStorage;
+import com.arnor4eck.storage.in_memory.InMemoryMonitoringTaskStorage;
+import com.arnor4eck.util.request.CreateMonitoringTaskRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.Optional;
+
+public class MonitoringTaskStorageTest {
+
+    private MonitoringTaskStorage monitoringTaskStorage;
+
+    @BeforeEach
+    public void setUp(){
+        monitoringTaskStorage = new InMemoryMonitoringTaskStorage();
+    }
+
+    @Test
+    public void testShouldSaveEntity(){
+        CreateMonitoringTaskRequest request =
+                new CreateMonitoringTaskRequest("test monitoring task",
+                        "https://square.github.io/okhttp/",
+                        5, ChronoUnit.HOURS);
+
+        monitoringTaskStorage.create(request);
+
+        MonitoringTask savedTask = monitoringTaskStorage.getById(1).get();
+
+        Assertions.assertNotNull(savedTask);
+        Assertions.assertEquals(savedTask.getName(), request.name());
+    }
+
+    @Test
+    public void testShouldDeleteEntity(){
+        CreateMonitoringTaskRequest request =
+                new CreateMonitoringTaskRequest("test monitoring task",
+                        "https://square.github.io/okhttp/",
+                        5, ChronoUnit.HOURS);
+
+        monitoringTaskStorage.create(request);
+
+        MonitoringTask savedTask = monitoringTaskStorage.getById(1).get();
+
+        Assertions.assertNotNull(savedTask);
+        Assertions.assertEquals(savedTask.getName(), request.name());
+
+        monitoringTaskStorage.deleteById(1);
+
+        Optional<MonitoringTask> deletedTask = monitoringTaskStorage.getById(1);
+
+        Assertions.assertEquals(deletedTask, Optional.empty());
+    }
+
+    @Test
+    public void shouldReturnCollection(){
+        final int SIZE = 5;
+
+        for(int i = 0; i < SIZE; i++){
+            monitoringTaskStorage.create(
+                    new CreateMonitoringTaskRequest("test monitoring task",
+                            "https://square.github.io/okhttp/",
+                            5, ChronoUnit.HOURS));
+        }
+
+        Collection<MonitoringTask> collection = monitoringTaskStorage.getAll();
+
+        Assertions.assertEquals(SIZE, collection.size());
+    }
+}
