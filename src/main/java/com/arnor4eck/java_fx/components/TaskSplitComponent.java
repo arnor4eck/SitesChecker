5a1d5349ed9edc815e1e6afaccd1eee67ba9b984
arnor4eck <KivalinNik@mail.ru>
2026-02-25 12:13:33 +0300

    added TaskSplitComponent

---
+++ src/main/java/com/arnor4eck/java_fx/components/TaskSplitComponent.java
@@ -0,0 +1,43 @@
+package com.arnor4eck.java_fx.components;
+
+import com.arnor4eck.java_fx.ApplicationConstants;
+import com.arnor4eck.java_fx.utils.SplitPaneUtils;
+import javafx.geometry.Orientation;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
+import lombok.Getter;
+
+@Getter
+public class TaskSplitComponent {
+
+    private final TasksComponent tasksComponent;
+    private final LogsComponent logsComponent;
+    private final SplitPane taskSplitPane;
+
+    public TaskSplitComponent(SplitPaneUtils splitPaneUtils) {
+        this.tasksComponent = new TasksComponent(splitPaneUtils);
+        this.logsComponent = new LogsComponent(splitPaneUtils);
+
+        this.taskSplitPane = setUpTasksPane();
+    }
+
+
+    private SplitPane setUpTasksPane(){
+        SplitPane splitPane = new SplitPane();
+        splitPane.setOrientation(Orientation.VERTICAL);
+        splitPane.setBackground(ApplicationConstants.BASE_BACKGROUND);
+        splitPane.setPadding(ApplicationConstants.BASE_INSETS);
+        splitPane.setDividerPositions(0.8);
+
+        // Сайты
+        Pane tasks = this.tasksComponent.getTasksPane();
+
+        // Логи
+        Pane logs = this.logsComponent.getLogsPane();
+
+        splitPane.getItems().addAll(tasks, logs);
+
+        return splitPane;
+    }
+
+}
