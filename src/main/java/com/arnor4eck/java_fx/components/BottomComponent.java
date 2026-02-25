2db11ac0033e58943dc20080a33bcb2cc1b03ad0
arnor4eck <KivalinNik@mail.ru>
2026-02-25 12:33:17 +0300

    added BottomComponent

---
+++ src/main/java/com/arnor4eck/java_fx/components/BottomComponent.java
@@ -0,0 +1,37 @@
+package com.arnor4eck.java_fx.components;
+
+import com.arnor4eck.java_fx.ApplicationConstants;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import lombok.Getter;
+
+@Getter
+public final class BottomComponent {
+
+    private final Label status;
+    private final Pane bottomPane;
+
+    public BottomComponent() {
+        this.status = setUpStatus();
+        this.bottomPane = setUpBottom();
+    }
+
+    private Label setUpStatus(){
+        Label l = new Label("Статус: Ожидание");
+        l.setStyle("-fx-text-fill: white;");
+
+        return l;
+    }
+
+    private Pane setUpBottom(){
+        HBox bottom = new HBox();
+        bottom.setPadding(new Insets(5));
+        bottom.setBackground(ApplicationConstants.BASE_BACKGROUND);
+
+        bottom.getChildren().addAll(this.status);
+
+        return bottom;
+    }
+}
