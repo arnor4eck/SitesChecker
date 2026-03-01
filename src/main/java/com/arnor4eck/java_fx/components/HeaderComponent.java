26b718652640d1ab6f2906bb5ada6aa826c8db10
arnor4eck <KivalinNik@mail.ru>
2026-03-01 22:25:15 +0300

    updated HeaderComponent: added callbacks

--- src/main/java/com/arnor4eck/java_fx/components/HeaderComponent.java
+++ src/main/java/com/arnor4eck/java_fx/components/HeaderComponent.java
@@ -1,6 +1,8 @@
 package com.arnor4eck.java_fx.components;
 
 import com.arnor4eck.java_fx.ApplicationConstants;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
@@ -17,23 +19,14 @@ import java.io.FileNotFoundException;
 @Getter
 public final class HeaderComponent {
 
-    private final Button listButton;
-    private final Button addButton;
-    private final Button docButton;
-    private final Button startButton;
     private final Pane headerPane;
 
-    public HeaderComponent() throws FileNotFoundException {
-        this.listButton = createHeaderButton("Список",
-                "/images/icon-list.png");
-        this.addButton = createHeaderButton("Добавить",
-                "/images/icon-plus.png");
-        this.docButton = createHeaderButton("Документация",
-                "/images/icon-book.png");
-        this.startButton = createHeaderButton("Старт",
-                "/images/icon-start.png");
-
-        this.headerPane = setUpHeader();
+    public HeaderComponent(EventHandler<ActionEvent> listButtonCallback,
+                           EventHandler<ActionEvent> addButtonCallback,
+                           EventHandler<ActionEvent> docButtonCallback,
+                           EventHandler<ActionEvent> startButtonCallback) throws FileNotFoundException {
+        this.headerPane = setUpHeader(listButtonCallback, addButtonCallback,
+                docButtonCallback, startButtonCallback);
     }
 
     private Button createHeaderButton(String name,
@@ -61,17 +54,41 @@ public final class HeaderComponent {
         return button;
     }
 
-    private Pane setUpHeader() throws FileNotFoundException {
+    private Pane setUpHeader(EventHandler<ActionEvent> listButtonCallback,
+                             EventHandler<ActionEvent> addButtonCallback,
+                             EventHandler<ActionEvent> docButtonCallback,
+                             EventHandler<ActionEvent> startButtonCallback) throws FileNotFoundException {
         HBox header = new HBox(); // общий HBox для всего хедера
 
         header.setBackground(ApplicationConstants.BASE_BACKGROUND);
         header.setPadding(ApplicationConstants.BASE_INSETS);
+
+        Button docButton = createHeaderButton("Документация",
+                "/images/icon-book.png");
+        docButton.setOnAction(docButtonCallback);
+
+        Button addButton = createHeaderButton("Добавить",
+                "/images/icon-plus.png");
+        addButton.setOnAction(addButtonCallback);
+
+        Button listButton = createHeaderButton("Список",
+                "/images/icon-list.png");
+        listButton.setOnAction(listButtonCallback);
 
         HBox leftGroup = new HBox(5, addButton, listButton, docButton); // левая группа кнопок
         leftGroup.setAlignment(Pos.CENTER_LEFT);
 
         Region spacer = new Region(); // спейсер, чтобы занимал оставшееся пространство
         HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        Button startButton = createHeaderButton("Старт",
+                "/images/icon-start.png");
+        startButton.setOnAction(actionEvent -> {
+            startButton.setText(
+                    startButton.getText().equals("Старт") ? "Стоп" : "Старт"
+            );
+            startButtonCallback.handle(actionEvent);
+        });
 
         HBox rightGroup = new HBox(startButton); // правая группа кнопок
         rightGroup.setAlignment(Pos.CENTER_RIGHT);
