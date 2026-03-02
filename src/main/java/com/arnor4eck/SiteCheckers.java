bdae8ac59d3b942a641a7788795d71c29cbc758e
arnor4eck <KivalinNik@mail.ru>
2026-03-02 21:31:15 +0300

    updated to singleton

--- src/main/java/com/arnor4eck/SiteCheckers.java
+++ src/main/java/com/arnor4eck/SiteCheckers.java
@@ -4,7 +4,16 @@ import com.arnor4eck.storage.in_memory.InMemoryMonitoringTaskStorage;
 import com.arnor4eck.storage.in_memory.InMemorySiteStatisticsStorage;
 
 public class SiteCheckers {
-    public static SiteCheckerApplication inMemory(){
+
+    private static class Handler{
+        public static SiteCheckerApplication APP = SiteCheckers.inMemory();
+    }
+
+    public static SiteCheckerApplication getInstance() {
+        return Handler.APP;
+    }
+
+    private static SiteCheckerApplication inMemory(){
         return new SiteCheckerApplication(new InMemoryMonitoringTaskStorage(),
                 new InMemorySiteStatisticsStorage());
     }
