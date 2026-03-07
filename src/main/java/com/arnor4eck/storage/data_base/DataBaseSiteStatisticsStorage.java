d97d76d83437af60824d2e2aaa84449e33d0e71f
arnor4eck <KivalinNik@mail.ru>
2026-03-07 14:38:34 +0300

    updated DataBaseSiteStatisticsStorage using execute query function

--- src/main/java/com/arnor4eck/storage/data_base/DataBaseSiteStatisticsStorage.java
+++ src/main/java/com/arnor4eck/storage/data_base/DataBaseSiteStatisticsStorage.java
@@ -3,6 +3,7 @@ package com.arnor4eck.storage.data_base;
 import com.arnor4eck.entity.SiteStatistics;
 import com.arnor4eck.java_fx.ApplicationUtils;
 import com.arnor4eck.storage.SiteStatisticsStorage;
+import com.arnor4eck.util.Logger;
 import com.arnor4eck.util.request.CreateSiteStatisticsRequest;
 
 import java.sql.*;
@@ -14,23 +15,24 @@ public class DataBaseSiteStatisticsStorage extends AbstractDataBaseStorage<SiteS
         super(db);
     }
 
+    private final Logger logger = Logger.getInstance();
+
     @Override
     public void create(CreateSiteStatisticsRequest request) {
         String query = "INSERT INTO site_statistics(check_time, http_code, hash, is_same_hash, monitoring_task_id) VALUES(?,?,?,?,?)";
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(query)){
+        try{
+            executeQuery(query, (ps) -> {
+                    ps.setString(1, request.checkTime().format(ApplicationUtils.formatter));
+                    ps.setShort(2, request.httpCode());
+                    ps.setString(3, request.hash());
+                    ps.setBoolean(4, request.isSameHash());
+                    ps.setLong(5, request.monitoringTaskId());
 
-            ps.setString(1, request.checkTime().format(ApplicationUtils.formatter));
-            ps.setShort(2, request.httpCode());
-            ps.setString(3, request.hash());
-            ps.setBoolean(4, request.isSameHash());
-            ps.setLong(5, request.monitoringTaskId());
-
-            ps.executeUpdate();
-
-        }catch(SQLException e){
-            throw new RuntimeException("Не удалось создать элемент статистики", e);
+                    return ps.executeUpdate();
+            });
+        }catch (SQLException e){
+            logger.error("Не удалось записать статистику: %s".formatted(e.getMessage()));
         }
     }
 
@@ -38,68 +40,78 @@ public class DataBaseSiteStatisticsStorage extends AbstractDataBaseStorage<SiteS
     public Optional<SiteStatistics> getLastStatisticsByMonitoringTaskId(long monitoringTaskId) {
         String query = "SELECT * FROM site_statistics WHERE monitoring_task_id = ? ORDER BY id DESC LIMIT 1;";
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(query)){
-            ps.setLong(1, monitoringTaskId);
+        try {
+            return executeQuery(query, ps -> {
+                ps.setLong(1, monitoringTaskId);
 
-            try(ResultSet rs = ps.executeQuery()){
-                if(rs.next())
-                    return Optional.of(extract(rs));
-            }
-            return Optional.empty();
+                try(ResultSet rs = ps.executeQuery()){
+                    if(rs.next())
+                        return Optional.of(extract(rs));
+                }
+                return Optional.empty();
+            });
         }catch(SQLException e){
-            throw new RuntimeException("Не найти элементы статистики", e);
+            logger.error("Не удалось найти элементы статистики: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось найти элементы статистики", e);
         }
     }
 
     @Override
     public Collection<SiteStatistics> getAllStatisticsByMonitoringTaskId(long monitoringTaskId) {
         String query = "SELECT * FROM site_statistics WHERE monitoring_task_id = ?";
-        List<SiteStatistics> siteStatisticsList = new LinkedList<>();
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(query)){
-            ps.setLong(1, monitoringTaskId);
+        try{
+            return executeQuery(query, ps -> {
+                List<SiteStatistics> siteStatisticsList = new LinkedList<>();
 
-            try(ResultSet rs = ps.executeQuery()){
-                while(rs.next())
-                    siteStatisticsList.add(extract(rs));
-            }
-            return siteStatisticsList;
-        }catch(SQLException e){
-            throw new RuntimeException("Не найти элементы статистики", e);
+                ps.setLong(1, monitoringTaskId);
+
+                try(ResultSet rs = ps.executeQuery()){
+                    while(rs.next())
+                        siteStatisticsList.add(extract(rs));
+                }
+
+                return siteStatisticsList;
+            });
+        } catch(SQLException e){
+            logger.error("Не удалось найти элементы статистики: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось найти элементы статистики", e);
         }
     }
 
     @Override
     public Optional<SiteStatistics> getById(long id) {
-        String statement = "SELECT * FROM site_statistics WHERE id = ?";
+        String query = "SELECT * FROM site_statistics WHERE id = ?";
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(statement)){
-            ps.setLong(1, id);
+        try{
+           return executeQuery(query, ps -> {
+               ps.setLong(1, id);
 
-            try(ResultSet rs = ps.executeQuery()){
-                if(rs.next())
-                    return Optional.of(extract(rs));
-            }
+               try(ResultSet rs = ps.executeQuery()){
+                   if(rs.next())
+                       return Optional.of(extract(rs));
+               }
 
-            return Optional.empty();
+               return Optional.empty();
+           });
         }catch(SQLException e){
-            throw new RuntimeException("Не найти элемент статистики", e);
+            logger.error("Не удалось найти элемент статистики: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось найти элемент статистики", e);
         }
     }
 
     @Override
     public void deleteById(long id) {
-        String statement = "DELETE FROM site_statistics WHERE id = ?";
+        String query = "DELETE FROM site_statistics WHERE id = ?";
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(statement)){
-            ps.setLong(1, id);
+        try{
+            executeQuery(query, ps -> {
+                ps.setLong(1, id);
 
-            ps.executeUpdate();
+                return ps.executeUpdate();
+            });
         }catch(SQLException e){
+            logger.error("Не удалить элемент статистики: %s".formatted(e.getMessage()));
             throw new RuntimeException("Не удалить элемент статистики", e);
         }
     }
@@ -107,20 +119,22 @@ public class DataBaseSiteStatisticsStorage extends AbstractDataBaseStorage<SiteS
     @Override
     public Collection<SiteStatistics> getAll() {
         String query = "SELECT * FROM site_statistics";
-        List<SiteStatistics> list = new LinkedList<>();
 
-        try(Connection con = this.getConnection();
-            Statement st = con.createStatement()){
+        try{
+            return Collections.unmodifiableCollection(executeQuery(query, ps -> {
+                List<SiteStatistics> list = new LinkedList<>();
 
-            try(ResultSet rs = st.executeQuery(query)){
-                while(rs.next())
-                    list.add(extract(rs));
-            }
+                try(ResultSet rs = ps.executeQuery(query)){
+                    while(rs.next())
+                        list.add(extract(rs));
+                }
+
+                return list;
+            }));
         } catch (SQLException e) {
-            throw new RuntimeException("Не удалось загрузить все записи", e);
+            logger.error("Не удалось загрузить все элементы статистики: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось загрузить все элементы статистики", e);
         }
-
-        return Collections.unmodifiableCollection(list);
     }
 
     @Override
