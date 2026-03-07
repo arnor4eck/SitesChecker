26d36d51bac18f8fad4d378e45abf92cd2273949
arnor4eck <KivalinNik@mail.ru>
2026-03-07 15:21:42 +0300

    added database application

--- src/main/java/com/arnor4eck/SiteCheckers.java
+++ src/main/java/com/arnor4eck/SiteCheckers.java
@@ -1,12 +1,15 @@
 package com.arnor4eck;
 
+import com.arnor4eck.storage.data_base.DataBase;
+import com.arnor4eck.storage.data_base.DataBaseMonitoringTaskStorage;
+import com.arnor4eck.storage.data_base.DataBaseSiteStatisticsStorage;
 import com.arnor4eck.storage.in_memory.InMemoryMonitoringTaskStorage;
 import com.arnor4eck.storage.in_memory.InMemorySiteStatisticsStorage;
 
 public class SiteCheckers {
 
     private static class Handler{
-        public static SiteCheckerApplication APP = SiteCheckers.inMemory();
+        public static SiteCheckerApplication APP = SiteCheckers.inDB();
     }
 
     public static SiteCheckerApplication getInstance() {
@@ -16,5 +19,13 @@ public class SiteCheckers {
     private static SiteCheckerApplication inMemory(){
         return new SiteCheckerApplication(new InMemoryMonitoringTaskStorage(),
                 new InMemorySiteStatisticsStorage());
+    }
+
+    private static SiteCheckerApplication inDB(){
+        DataBase db = new DataBase("jdbc:sqlite:checker-app.db");
+        Runtime.getRuntime().addShutdownHook(new Thread(db::close));
+
+        return new SiteCheckerApplication(new DataBaseMonitoringTaskStorage(db),
+                new DataBaseSiteStatisticsStorage(db));
     }
 }
