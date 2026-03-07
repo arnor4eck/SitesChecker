6749b241a99c8aa4f3bcf11876a4a4a7ddf68539
arnor4eck <KivalinNik@mail.ru>
2026-03-07 15:08:09 +0300

    updated DataBaseMonitoringTaskStorage using executeQueries

--- src/main/java/com/arnor4eck/storage/data_base/DataBaseMonitoringTaskStorage.java
+++ src/main/java/com/arnor4eck/storage/data_base/DataBaseMonitoringTaskStorage.java
@@ -3,6 +3,7 @@ package com.arnor4eck.storage.data_base;
 import com.arnor4eck.entity.MonitoringTask;
 import com.arnor4eck.java_fx.ApplicationUtils;
 import com.arnor4eck.storage.MonitoringTaskStorage;
+import com.arnor4eck.util.Logger;
 import com.arnor4eck.util.request.CreateMonitoringTaskRequest;
 
 import java.sql.*;
@@ -10,6 +11,9 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 public class DataBaseMonitoringTaskStorage extends AbstractDataBaseStorage<MonitoringTask> implements MonitoringTaskStorage {
+
+    private final Logger logger = Logger.getInstance();
+
     public DataBaseMonitoringTaskStorage(DataBase db) {
         super(db);
     }
@@ -27,28 +31,29 @@ public class DataBaseMonitoringTaskStorage extends AbstractDataBaseStorage<Monit
 
     @Override
     public MonitoringTask create(CreateMonitoringTaskRequest request) {
-        String statement = "INSERT INTO monitoring_task(url, name, period, unit, next_check_time) VALUES(?, ?, ?, ?, ?) RETURNING id";
+        String query = "INSERT INTO monitoring_task(url, name, period, unit, next_check_time) VALUES(?, ?, ?, ?, ?) RETURNING id";
 
-        MonitoringTask monitoringTask = new MonitoringTask(-1, request.url(),
-                request.name(), request.period(), request.unit());
+        try{
+           return executeQuery(query, ps -> {
+                MonitoringTask monitoringTask = new MonitoringTask(-1, request.url(),
+                        request.name(), request.period(), request.unit());
 
-        try(Connection con = this.getConnection();
-            PreparedStatement preparedStatement = con.prepareStatement(statement)){
+                ps.setString(1, monitoringTask.getUrl());
+                ps.setString(2, monitoringTask.getName());
+                ps.setLong(3, monitoringTask.getPeriod());
+                ps.setString(4, ApplicationUtils.parseChronoUnitToString(monitoringTask.getUnit()));
+                ps.setString(5, monitoringTask.getNextCheckTime().format(ApplicationUtils.formatter));
 
-            preparedStatement.setString(1, monitoringTask.getUrl());
-            preparedStatement.setString(2, monitoringTask.getName());
-            preparedStatement.setLong(3, monitoringTask.getPeriod());
-            preparedStatement.setString(4, ApplicationUtils.parseChronoUnitToString(monitoringTask.getUnit()));
-            preparedStatement.setString(5, monitoringTask.getNextCheckTime().format(ApplicationUtils.formatter));
-
-            try(ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    monitoringTask.setId(resultSet.getLong("id"));
-                    return monitoringTask;
+                try(ResultSet resultSet = ps.executeQuery()) {
+                    if (resultSet.next()) {
+                        monitoringTask.setId(resultSet.getLong("id"));
+                        return monitoringTask;
+                    }
+                    throw new SQLException("Не удалось получить ID");
                 }
-                throw new SQLException("Не удалось получить ID");
-            }
+            });
         } catch (Exception e) {
+            logger.error("Ошибка при создании задачи мониторинга: %s".formatted(e.getMessage()));
             throw new RuntimeException("Ошибка при создании задачи мониторинга", e);
         }
     }
@@ -57,40 +62,42 @@ public class DataBaseMonitoringTaskStorage extends AbstractDataBaseStorage<Monit
     public MonitoringTask updateExistingMonitoringTask(MonitoringTask monitoringTask) {
         String query = "UPDATE monitoring_task SET url = ?, name = ?, period = ?, unit = ?, next_check_time = ? WHERE id = ?";
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(query)){
+        try{
+            return executeQuery(query, ps -> {
+                ps.setString(1, monitoringTask.getUrl());
+                ps.setString(2, monitoringTask.getName());
+                ps.setLong(3, monitoringTask.getPeriod());
+                ps.setString(4, ApplicationUtils.parseChronoUnitToString(monitoringTask.getUnit()));
+                ps.setString(5, monitoringTask.getNextCheckTime().format(ApplicationUtils.formatter));
+                ps.setLong(6, monitoringTask.getId());
 
-            ps.setString(1, monitoringTask.getUrl());
-            ps.setString(2, monitoringTask.getName());
-            ps.setLong(3, monitoringTask.getPeriod());
-            ps.setString(4, ApplicationUtils.parseChronoUnitToString(monitoringTask.getUnit()));
-            ps.setString(5, monitoringTask.getNextCheckTime().format(ApplicationUtils.formatter));
-            ps.setLong(6, monitoringTask.getId());
+                ps.executeUpdate();
 
-            ps.executeUpdate();
-
-            return monitoringTask;
+                return monitoringTask;
+            });
         }catch(SQLException e){
+            logger.error("Не удалось обновить задачу: %s".formatted(e.getMessage()));
             throw new RuntimeException("Не удалось обновить задачу", e);
         }
     }
 
     @Override
     public Optional<MonitoringTask> getById(long id) {
-        String statement = "SELECT * FROM monitoring_task WHERE id = ?";
+        String query = "SELECT * FROM monitoring_task WHERE id = ?";
 
-        try(Connection con = this.getConnection();
-            PreparedStatement ps = con.prepareStatement(statement)){
-            ps.setLong(1, id);
+        try{
+            return executeQuery(query, ps -> {
+                ps.setLong(1, id);
 
-            try(ResultSet rs = ps.executeQuery()){
-                if(rs.next())
-                    return Optional.of(extract(rs));
-            }
-
-            return Optional.empty();
-        }catch(SQLException e){
-            throw new RuntimeException("Не найти задачу", e);
+                try(ResultSet rs = ps.executeQuery()){
+                    if(rs.next())
+                        return Optional.of(extract(rs));
+                }
+                return Optional.empty();
+            });
+        } catch(SQLException e){
+            logger.error("Не удалось найти задачу: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось найти задачу", e);
         }
     }
 
@@ -104,27 +111,29 @@ public class DataBaseMonitoringTaskStorage extends AbstractDataBaseStorage<Monit
 
             ps.executeUpdate();
         }catch(SQLException e){
-            throw new RuntimeException("Не удалить задачу", e);
+            logger.error("Не удалось удалить задачу: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось удалить задачу", e);
         }
     }
 
     @Override
     public Collection<MonitoringTask> getAll() {
-        String statement = "SELECT * FROM monitoring_task";
+        String query = "SELECT * FROM monitoring_task";
 
-        List<MonitoringTask> monitoringTasks = new LinkedList<>();
+        try{
+            return executeQuery(query, ps -> {
+                List<MonitoringTask> monitoringTasks = new LinkedList<>();
 
-        try(Connection con = this.getConnection();
-            Statement st = con.createStatement()){
+                try(ResultSet rs = ps.executeQuery()){
+                    while(rs.next())
+                        monitoringTasks.add(extract(rs));
+                }
 
-            try(ResultSet rs = st.executeQuery(statement)){
-                while(rs.next())
-                    monitoringTasks.add(extract(rs));
-            }
-
-            return monitoringTasks;
+                return monitoringTasks;
+            });
         } catch (Exception e) {
-            throw new RuntimeException("Не удалось выполнить запрос", e);
+            logger.error("Не удалось найти задачи: %s".formatted(e.getMessage()));
+            throw new RuntimeException("Не удалось найти задачи", e);
         }
     }
 }
